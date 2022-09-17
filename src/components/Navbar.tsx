e44dd014261a789ca406b4db634ab92abba34e79
Rodrigo Lourenço <rodrigofplourenco2002@gmail.com>
2022-09-17 18:56:41 +0100

    Navbar update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ export default function Navbar({ changeWiredType }: INavbarProps) {
   return (
     <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
-        <a href="https://flowbite.com/" className="flex items-center">
+        <a href="#" className="flex items-center">
             <img src="/images/ativador5.png" className="mr-3 h-6 sm:h-9" alt="Wired Docs Logo" />
             <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Wired Docs</span>
         </a>

6ad8d697c30378cf010790d748c3c7e3820dfe4a
Rodrigo Lourenço <rodrigofplourenco2002@gmail.com>
2022-11-29 18:21:13 +0000

    [CLOSE] Closed website

--- src/App.tsx
+++ src/App.tsx
@@ -10,31 +10,40 @@ import Footer from './components/Footer';
 
 
 function App() {
-  const allWireds: IWireds = wireds;
-  const [wiredType, setWiredType] = useState<IWiredType>(nav[0].wiredType);
+  // const allWireds: IWireds = wireds;
+  // const [wiredType, setWiredType] = useState<IWiredType>(nav[0].wiredType);
 
-  const filteredWireds = allWireds[wiredType];
+  // const filteredWireds = allWireds[wiredType];
 
-  function getPreImageName() {
-    if (wiredType === "ativadores") return "ativador";
-    else if (wiredType === "efeitos") return "efeito";
-    else if (wiredType === "condicoes positivas") return "cond_positiva";
-    else if (wiredType === "condicoes negativas") return "cond_negativa";
-    else if (wiredType === "extras") return "extra";
-    else if (wiredType === "operadores") return "operador";
-    else if (wiredType === "iron wireds") return "iron";
-    else if (wiredType === "comparadores") return "comparador";
-  }
+  // function getPreImageName() {
+  //   if (wiredType === "ativadores") return "ativador";
+  //   else if (wiredType === "efeitos") return "efeito";
+  //   else if (wiredType === "condicoes positivas") return "cond_positiva";
+  //   else if (wiredType === "condicoes negativas") return "cond_negativa";
+  //   else if (wiredType === "extras") return "extra";
+  //   else if (wiredType === "operadores") return "operador";
+  //   else if (wiredType === "iron wireds") return "iron";
+  //   else if (wiredType === "comparadores") return "comparador";
+  // }
 
-  function changeWiredType(wiredType: IWiredType) {
-    setWiredType(wiredType);
-  }
+  // function changeWiredType(wiredType: IWiredType) {
+  //   setWiredType(wiredType);
+  // }
 
   return ( 
     <>
-      <Navbar changeWiredType={changeWiredType} />
+      <div className='flex flex-col h-screen w-full justify-center items-center'>
+        <h1>
+          Pedimos desculpa, o site foi encerrado, agradecemos a todos os que fizeram uso do nosso website
+        </h1>
+        <br />
+        <h2>
+          Se quiser ver outros projetos meus, acesse <a target="_blank" href='https://github.com/rodrigofplourenco' className='underline font-bold'>aqui</a>
+        </h2>
+      </div>
+      {/* <Navbar changeWiredType={changeWiredType} /> */}
 
-      <div className="px-6 py-8 grid grid-cols-5 gap-6 mx-auto">
+      {/* <div className="px-6 py-8 grid grid-cols-5 gap-6 mx-auto">
         {filteredWireds.map((wired, index) => (
           <div key={wired.name} className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
               <a href="#" className='flex justify-center my-6'>
@@ -49,9 +58,9 @@ function App() {
               </div>
           </div>
         ))}
-      </div>
+      </div> */}
 
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 }
